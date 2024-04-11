c4546394576828e42c2d4b9fabe6fe36c5bae26f
williammtan <william@tan.id>
2024-04-11 22:39:10 +0700

    remove conda during install

--- install_script.sh
+++ install_script.sh
@@ -1,6 +1,2 @@
-conda init bash
-conda create -n alma-r python=3.11 -y
-conda activate alma-r
-
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 bash install_alma.sh
