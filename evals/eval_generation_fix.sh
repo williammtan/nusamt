27aaea3668eeab6e4b99dc51fb8a5e5069e0fbf5
will tan <william@tan.id>
2024-07-01 14:32:43 +0000

    add new eval script

---
+++ evals/eval_generation_fix.sh
@@ -0,0 +1,25 @@
+OUTPUT_DIR=${1}
+TEST_PAIRS=${2:-"ban-en,en-ban"}
+DATA_DIR=${3}
+
+## Evaluation
+for pair in ${TEST_PAIRS//,/ }; do
+    src=$(echo ${pair} | cut -d "-" -f 1)
+    tgt=$(echo ${pair} | cut -d "-" -f 2)
+    TOK="13a"
+
+    tgt_path=${DATA_DIR}/${src}${tgt}/test.${src}-${tgt}.${tgt}
+    output_path=${OUTPUT_DIR}/test-${src}-${tgt}
+    echo "${tgt_path} ${output_path}"
+
+    sacrebleu -tok ${TOK} -w 2 ${tgt_path} < ${output_path} > ${output_path}.bleu
+    cat ${output_path}.bleu
+done
+
+for pair in ${TEST_PAIRS//,/ }; do
+    src=$(echo ${pair} | cut -d "-" -f 1)
+    tgt=$(echo ${pair} | cut -d "-" -f 2)
+    echo "---------------------------${src}-${tgt}-------------------------------"
+    output_path=${OUTPUT_DIR}/test-${src}-${tgt}
+    cat ${output_path}.bleu
+done
