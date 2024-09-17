2fbeb50d5da23ac3fbfa6ef807876106423c9a66
williammtan <william@tan.id>
2024-09-17 18:43:02 +0700

    anon

--- runs/run-bt-train.sh
+++ runs/run-bt-train.sh
@@ -1,34 +1,43 @@
-OUTPUT_DIR=${1:-"./alma-7b-parallel-ft"}
-pairs=${2:-"de-en,cs-en,is-en,zh-en,ru-en,en-de,en-cs,en-is,en-zh,en-ru"}
-export HF_DATASETS_CACHE=".cache/huggingface_cache/datasets"
-export TRANSFORMERS_CACHE=".cache/models/"
+MODEL_PATH="$1"         
+pairs="$2"          
+DATA_DIR="$3"  
+OUTPUT_DIR="$4"                        
+OUTPUT_EVAL_DIR="$5"
+LORA_RANK=${3:-"16"}
+export CXX=g++-11
+export CC=gcc-11
+export LD=g++-11
 
 # random port between 30000 and 50000
 port=$(( RANDOM % (50000 - 30000 + 1 ) + 30000 ))
+
 accelerate launch --main_process_port ${port} --config_file configs/deepspeed_train_config.yaml \
      run_llmmt.py \
-    --model_name_or_path haoranxu/ALMA-7B-Pretrain \
-    --mmt_data_path ./human_written_data/ \
+    --model_name_or_path Yellow-AI-NLP/komodo-7b-base \
+    --torch_dtype "bfloat16" \
+    --mmt_data_path  ${DATA_DIR} \
+    --use_peft \
+    --peft_model_id ${MODEL_PATH} \
+    --lora_rank ${LORA_RANK} \
     --do_train \
     --do_eval \
     --do_predict \
     --language_pairs ${pairs} \
     --load_best_model_at_end \
-    --low_cpu_mem_usage \
-    --fp16 \
-    --learning_rate 2e-5 \
+    --bf16 \
+    --learning_rate 2e-3 \
     --weight_decay 0.01 \
     --gradient_accumulation_steps 4 \
     --lr_scheduler_type inverse_sqrt \
     --warmup_ratio 0.01 \
     --ignore_pad_token_for_loss \
     --ignore_prompt_token_for_loss \
-    --per_device_train_batch_size 4 \
-    --per_device_eval_batch_size 4 \
+    --per_device_train_batch_size 2 \
+    --per_device_eval_batch_size 2 \
     --evaluation_strategy steps \
-    --eval_steps 0.1 \
+    --eval_steps 0.05 \
     --save_strategy steps \
-    --save_steps 0.1 \
+    --save_steps 0.05 \
     --save_total_limit 1 \
     --logging_strategy steps \
     --logging_steps 0.05 \
@@ -42,8 +51,6 @@ accelerate launch --main_process_port ${port} --config_file configs/deepspeed_tr
     --overwrite_output_dir \
     --num_beams 5 \
     --ddp_timeout 999999 \
-    --report_to none \
-    --overwrite_cache 
+    --report_to wandb \
 
-## Evaluation (BLEU, COMET)
-bash ./evals/eval_generation.sh ${OUTPUT_DIR} ${pairs}
+bash flores-eval.sh ./data/bt-align/
